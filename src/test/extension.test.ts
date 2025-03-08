d6898c60bb805aa5a9a07c5cd35384af48bdf25b
Matt Brannon <mattbrannon.dev@gmail.com>
2025-03-08 16:46:26 -0600

    refactor(workflow): add CI workflow for pull request validation and testing

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,31 @@
-import * as assert from 'assert';
+import assert from 'node:assert';
+import path from 'node:path';
+import vscode from 'vscode';
+import DocumentFormatter from '../formatter';
+import packageJson from '../../package.json';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+// import Cache from '../utils/cache'
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+  const extensionId = `${packageJson.publisher}.${packageJson.name}`;
+  test('Extension is activated', async () => {
+    const extension = vscode.extensions.getExtension(extensionId);
+    await extension?.activate();
+    assert.ok(extension?.isActive);
+  });
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+  test('Formats a document', async () => {
+    const filepath = vscode.Uri.file(
+      path.resolve(process.cwd(), 'src/test/sample-file.ts'),
+    );
+    const document = await vscode.workspace.openTextDocument(filepath);
+    const editor = await vscode.window.showTextDocument(document);
+    await editor.edit((editBuilder) => {
+      editBuilder.insert(new vscode.Position(0, 0), 'var foo = 1');
+    });
+    const expected = 'const foo = 1;\n';
+    const result = await DocumentFormatter.formatDocument(document);
+
+    assert.equal(expected, result[0].newText);
+  });
 });
